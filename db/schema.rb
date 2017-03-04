9a2303b227d1c6b5e93b8d43d1c8255472e42171
Bence Kiss <kiss.bence.balint@gmail.com>
2017-03-04 11:42:24 -0500

    Added form for creating account and fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304151715) do
+ActiveRecord::Schema.define(version: 20170304154503) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "email",            null: false
@@ -30,6 +30,23 @@ ActiveRecord::Schema.define(version: 20170304151715) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["email"], name: "index_accounts_on_email", unique: true
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.string   "to_number"
+    t.string   "from_number"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.text     "template"
+    t.integer  "message_id"
+    t.integer  "account_id"
+    t.datetime "schedule_time"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
