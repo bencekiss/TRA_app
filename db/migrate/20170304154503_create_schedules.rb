3a99cc61db99304f1d97004b710bb0cc6d9abd78
Bence Kiss <kiss.bence.balint@gmail.com>
2017-03-04 10:58:38 -0500

    Generated scaffolds for Schedules and Messages and added nested routes and populated models for them

---
+++ db/migrate/20170304154503_create_schedules.rb
@@ -0,0 +1,12 @@
+class CreateSchedules < ActiveRecord::Migration[5.0]
+  def change
+    create_table :schedules do |t|
+      t.text :template,
+      t.integer :message_id,
+      t.integer :account_id,
+      t.datetime :schedule_time,
+
+      t.timestamps
+    end
+  end
+end
