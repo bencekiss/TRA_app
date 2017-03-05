897916c96714f72191abe4b7978386662702eea9
Alex Flint <me@alexf.ca>
2017-03-05 08:14:14 -0500

    minor changes to views and added twilio_num to secrets

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,2 @@
-APP_NUMBER = "+16474960471"
+APP_NUMBER = Rails.application.secrets.twilio_num
 TWILIO_CLIENT = Twilio::REST::Client.new(Rails.application.secrets.twilio_sid, Rails.application.secrets.twilio_token)
