6c24fdcab262f9056e776323db93309b615d7146
Bence Kiss <kiss.bence.balint@gmail.com>
2017-03-05 12:25:31 -0500

    Fixed the replies so that sending next stage notifications change previous ones to no_reply. If replied doesnt run the next notifications

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -40,6 +40,7 @@ class Schedule < ApplicationRecord
         ms_time = message.created_at.utc.min
         if message && schedule && ms_time == sc_time && message.status == "delivered" && diff >= 1 && diff < 2
           Message.send_message(message.account.phone, schedule.create_primary_rescue, schedule.id)
+          message.update(status: "no_reply")
         end
       end
     end
@@ -71,69 +72,105 @@ class Schedule < ApplicationRecord
       schedule.messages.each do |message|
         diff = Time.now.min.to_i - message.created_at.min.to_i
         ms_time = message.created_at.utc.min
-        if message && schedule && ms_time == sc_time && message.status == "delivered" && diff >= 2 && diff < 3
-          Message.send_message(message.account.secondary_phone, schedule.create_secondary_template, schedule.id)
+        if message && schedule && ms_time == sc_time && message.status == "no_reply" && diff >= 2 && diff < 3
+          last_message = schedule.messages.where("created_at > ? AND status = ?", schedule.schedule_time, "delivered").order("created_at DESC").first
+          if last_message
+            last_message.update(status: "no_reply")
+            Message.send_message(message.account.secondary_phone, schedule.create_secondary_template, schedule.id)
+            # Message.send_message(message.account.phone, schedule.create_primary_notification, message.schedule.id)
+          end
         end
       end
     end
   end
 
   def self.send_secondary_rescue
-    Message.update_old_messages
-    messages = Message.where("created_at >= ?", Time.now.utc.beginning_of_day-1)
-    messages.each do |message|
-      if message.status == 'delivered' && (Time.now.hour.to_i - message.created_at.hour.to_i) >= 1
-        Message.send_message(message.account.secondary_phone, create_secondary_rescue, message.schedule.id)
-        Message.send_message(message.account.phone, create_primary_notification, message.schedule.id)
+    # Message.update_old_messages
+    # messages = Message.where("created_at >= ?", Time.now.utc.beginning_of_day-1)
+    # messages.each do |message|
+    #   if message.status == 'delivered' && (Time.now.hour.to_i - message.created_at.hour.to_i) >= 1
+    #     Message.send_message(message.account.secondary_phone, create_secondary_rescue, message.schedule.id)
+    #     Message.send_message(message.account.phone, create_primary_notification, message.schedule.id)
+    #   end
+    # end
+    Schedule.all.each do |schedule|
+      sc_time = schedule.schedule_time.utc.min
+      schedule.messages.each do |message|
+        diff = Time.now.min.to_i - message.created_at.min.to_i
+        ms_time = message.created_at.utc.min
+        if message && schedule && ms_time == sc_time && message.status == "no_reply" && diff >= 3 && diff < 4
+          last_message = schedule.messages.where("created_at > ? AND status = ?", schedule.schedule_time, "delivered").order("created_at DESC").first
+          if last_message
+            last_message.update(status: "no_reply")
+            Message.send_message(message.account.secondary_phone, schedule.create_secondary_rescue, message.schedule.id)
+          end
+        end
       end
     end
   end
 
   def self.send_emergency_message
-    Message.update_old_messages
-    messages = Message.where("created_at >= ?", Time.now.utc.beginning_of_day-1)
-    messages.each do |message|
-      if message.status == 'delivered' && (Time.now.hour.to_i - message.created_at.hour.to_i) >= 1
-        Message.send_message(message.account.emergency_phone, create_emergency_template, message.schedule.id)
-        Message.send_message(message.account.phone, create_primary_emergency_notification, message.schedule.id)
+    # Message.update_old_messages
+    # messages = Message.where("created_at >= ?", Time.now.utc.beginning_of_day-1)
+    # messages.each do |message|
+    #   if message.status == 'delivered' && (Time.now.hour.to_i - message.created_at.hour.to_i) >= 1
+    #     Message.send_message(message.account.emergency_phone, create_emergency_template, message.schedule.id)
+    #     Message.send_message(message.account.phone, create_primary_emergency_notification, message.schedule.id)
+    #   end
+    # end
+    Schedule.all.each do |schedule|
+      sc_time = schedule.schedule_time.utc.min
+      schedule.messages.each do |message|
+        # previous = schedule.messages.where("created_at > ? AND status = ?", schedule.schedule_time.min + 3, "delivered").first
+        # if schedule.schedule_time + 4
+        diff = Time.now.min.to_i - message.created_at.min.to_i
+        ms_time = message.created_at.utc.min
+        if message && schedule && ms_time == sc_time && message.status == "no_reply" && diff >= 4 && diff < 5
+          last_message = schedule.messages.where("created_at > ? AND status = ?", schedule.schedule_time, "delivered").order("created_at DESC").first
+          if last_message
+            last_message.update(status: "no_reply")
+            Message.send_message(message.account.emergency_phone, schedule.create_emergency_template, message.schedule.id)
+            # Message.send_message(message.account.phone, schedule.create_primary_emergency_notification, message.schedule.id)
+          end
+        end
       end
     end
   end
 
   def create_primary_template
-    body = "Hi there #{self.account.name}! Have you seen if #{self.account.patient_name} is doing ok? Reply YES to confirm."
+    body = "prim:Hi there #{self.account.name}! Have you seen if #{self.account.patient_name} is doing ok? Reply YES to confirm."
     body
   end
 
   def create_primary_rescue
-    body = "Hi there #{self.account.name}! We haven't heard back from you. Are you ok? Reply YES to confirm."
+    body = "prim_resc:Hi there #{self.account.name}! We haven't heard back from you. Are you ok? Reply YES to confirm."
     body
   end
 
   def create_primary_notification
-    body = "Hi there #{self.account.name}! Because we haven't heard from you, we've messaged #{self.account.secondary_name}. Upon
+    body = "prim_sec_not: Hi there #{self.account.name}! Because we haven't heard from you, we've messaged #{self.account.secondary_name}. Upon
     recieving this message please contact #{self.account.secondary_name}!"
     body
   end
 
   def create_primary_emergency_notification
-    body = "Hi there #{self.account.name}! Because we haven't heard from you, or #{self.account.secondary_name}, we've contacted #{self.account.emergency_name}!
+    body = "prim_emerg: Hi there #{self.account.name}! Because we haven't heard from you, or #{self.account.secondary_name}, we've contacted #{self.account.emergency_name}!
     Upon recieving this message please contact #{self.account.emergency_name}!"
     body
   end
 
   def create_secondary_template
-    body = "Hi there #{self.account.secondary_name}! Please check on #{self.account.patient_name}. We haven't heard back from #{self.account.name}"
+    body = "sec: Hi there #{self.account.secondary_name}! Please check on #{self.account.patient_name}. We haven't heard back from #{self.account.name}"
     body
   end
 
   def create_secondary_rescue
-    body = "Hi there #{self.account.secondary_name}! We haven't heard back from you. Are you ok? Reply YES to confirm."
+    body = "sec_resc: Hi there #{self.account.secondary_name}! We haven't heard back from you. Are you ok? Reply YES to confirm."
     body
   end
 
   def create_emergency_template
-    body = "Hi there #{self.account.emergency_name}. There's an emergency with #{self.account.patient_name} at #{self.account.patient_address}. You can reach him at #{self.account.patient_number}."
+    body = "EMERGENCY: Hi there #{self.account.emergency_name}. There's an emergency with #{self.account.patient_name} at #{self.account.patient_address}. You can reach him at #{self.account.patient_number}. In case of a serious emergency please call 911"
     body
   end
 
