24ab37e1fa6b3b5c339fe7b3d62319b07f34d31c
Yuri <yuri.manahan@gmail.com>
2017-03-05 15:37:18 -0500

    Added Gee's changes to form JS and CSS

--- app/assets/javascripts/accounts/edit.js
+++ app/assets/javascripts/accounts/edit.js
@@ -1,27 +1,47 @@
 $(document).on("turbolinks:load",function(){
-  $('.prime').on('click', function(){
-    $('.secondary-caregiver').fadeOut();
-    $('.caregivers').fadeIn();
-    $('.patient').fadeOut();
-  });
-  $('.secondary').on('click', function(){
-    $('.secondary-caregiver').fadeIn();
-    $('.caregivers').fadeOut();
-    $('.patient').fadeOut();
-  });
-  $('.emerge').on('click', function(){
-    $('.secondary-caregiver').fadeOut();
-    $('.caregivers').fadeOut();
-    $('.patient').fadeIn();
-  });
-  $('.secondary-next').on('click', function(){
-    $('.secondary-caregiver').fadeIn();
-    $('.caregivers').fadeOut();
-    $('.patient').fadeOut();
-  });
-  $('.emerge-next').on('click', function(){
-    $('.secondary-caregiver').fadeOut();
-    $('.caregivers').fadeOut();
-    $('.patient').fadeIn();
-  });
+ $('.third-back').hide();
+ $('.prime').on('click', function(){
+   $('.secondary-caregiver').fadeOut();
+   $('.caregivers').fadeIn();
+   $('.patient').fadeOut();
+   $('.third-back').hide();
+ });
+
+ $('.secondary').on('click', function(){
+   $('.secondary-caregiver').fadeIn();
+   $('.caregivers').fadeOut();
+   $('.patient').fadeOut();
+ });
+ $('.emerge').on('click', function(){
+   $('.secondary-caregiver').fadeOut();
+   $('.caregivers').fadeOut();
+   $('.patient').fadeIn();
+ });
+ $('.secondary-next').on('click', function(){
+   $('.secondary-caregiver').fadeIn();
+   $('.caregivers').fadeOut();
+   $('.patient').fadeOut();
+   $('.third-back').show();
+   $('.secondary-back').hide();
+ });
+   $('.third-back').on('click', function(){
+     $('.secondary-caregiver').fadeOut();
+     $('.caregivers').fadeIn();
+     $('.patient').fadeOut();
+
+ });
+ $('.emerge-next').on('click', function(){
+   $('.secondary-caregiver').fadeOut();
+   $('.caregivers').fadeOut();
+   $('.patient').fadeIn();
+   $('.third-back').hide();
+ });
+
+ $('.fourth-back').on('click', function(){
+   $('.secondary-caregiver').fadeIn();
+   $('.caregivers').fadeOut();
+   $('.patient').fadeOut();
+   $('.third-back').show();
+ });
+
 });
